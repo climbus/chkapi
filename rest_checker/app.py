34a735c93e6c92c62c144b55e5817f2ee320d840
climbus <climbus@gmail.com>
2021-10-16 19:40:43 +0200

    changing url after start

--- rest_checker/app.py
+++ rest_checker/app.py
@@ -9,20 +9,17 @@ from rich.panel import Panel
 from textual.app import App
 from textual.views import GridView
 from textual.widget import Reactive
-from textual.widgets import Button, ButtonPressed, Placeholder, ScrollView
+from textual.widgets import Button, ButtonPressed, ScrollView
 from textual_inputs import TextInput
 
 
 class URLField(TextInput):
-    url: str
-
     def __init__(self, url):
-        self.url = url
         super().__init__(value=url, title="URL")
 
-    def set_url(self, url):
-        self.url = url
-        self.value = url
+    @property
+    def url(self):
+        return self.value
 
 
 class URLButton(Button, can_focus=True):
@@ -51,30 +48,36 @@ class URLButton(Button, can_focus=True):
 
 
 class URLView(GridView):
-    url: str
+    url_field: URLField
 
     def __init__(self, url: str = "") -> None:
-        self.url = url
         super().__init__()
+        self.url_field = URLField(url)
 
     async def on_mount(self):
         self.grid.add_column("url")
         self.grid.add_column("button", size=10)
         self.grid.add_row("main", size=3)
         self.grid.add_areas(url="url,main", button="button,main")
-        self.grid.place(url=URLField(self.url))
+        self.grid.place(url=self.url_field)
         self.grid.place(button=URLButton(label="GO"))
+
+    @property
+    def url(self):
+        return self.url_field.url
 
 
 class RestChecker(App):
     async def on_mount(self):
-        url_view = URLView(self.url)
+        self.url_view = URLView(self.url)
         self.body = ScrollView()
-        await self.view.dock(url_view, size=3, edge="top")
+        await self.view.dock(self.url_view, size=3, edge="top")
         await self.view.dock(self.body, edge="bottom")
 
-    async def load_url(self):
-        content = self._get_url_content()
+    async def load_url(self, url):
+        self.log(f"Loading url {url}")
+        content = self._get_url_content(url)
+        self.log(f"Response: {content}")
         await self.body.update(JSON(content.text))
 
     async def on_load(self):
@@ -88,13 +91,17 @@ class RestChecker(App):
     async def handle_button_pressed(self, message: ButtonPressed) -> None:
         button = cast(URLButton, message.sender)
         button.has_focus = False
-        await self.load_url()
+        await self.load_url(self.url_view.url)
 
-    def _get_url_content(self):
-        return request("get", self.url)
+    def _get_url_content(self, url):
+        return request("get", url)
 
     def _get_url_from_attrs(self) -> str:
-        return sys.argv[1]
+        if len(sys.argv) > 1:
+            return sys.argv[1]
+        else:
+            return ""
 
 
-RestChecker.run(title="Rest Checker", log="textual.log")
+if __name__ == "__main__":
+    RestChecker.run(title="Rest Checker", log="textual.log")
