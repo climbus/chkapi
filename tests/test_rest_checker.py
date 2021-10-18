c0514b5127e8c29acb7243e99c5192d460002101
climbus <climbus@gmail.com>
2021-10-18 15:53:46 +0200

    test for quit by q key

--- tests/test_rest_checker.py
+++ tests/test_rest_checker.py
@@ -16,9 +16,21 @@ async def press_ctrl_c():
     pyautogui.hotkey("ctrl", "c")
 
 
+async def press(key):
+    pyautogui.press(key)
+
+
 @pytest.mark.asyncio
 async def test_app_quits_on_ctr_c():
     app, _ = create_app()
 
     await asyncio.gather(app.process_messages(), press_ctrl_c())
+    assert True
+
+
+@pytest.mark.asyncio
+async def test_app_quits_on_q():
+    app, _ = create_app()
+
+    await asyncio.gather(app.process_messages(), press("q"))
     assert True
