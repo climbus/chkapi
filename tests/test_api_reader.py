9c56dfee9dd0dd20533580ec4b78f7f764390ad0
climbus <climbus@gmail.com>
2021-10-20 08:57:40 +0200

    test for getting content (happy path)

--- tests/test_api_reader.py
+++ tests/test_api_reader.py
@@ -1,10 +1,25 @@
 import pytest
+from pytest_httpserver.httpserver import HTTPServer
 
-from rest_checker.api_reader import AsyncAPIReader
+from rest_checker.api_reader import URL, AsyncAPIReader
 from rest_checker.exceptions import BadUrlException
+
 
 @pytest.mark.asyncio
 async def test_none_as_url_should_raise_bad_url_exception():
     reader = AsyncAPIReader()
     with pytest.raises(BadUrlException):
-        await reader.read_url(None) # type: ignore
+        await reader.read_url(None)  # type: ignore
+
+
+@pytest.mark.asyncio
+async def test_should_get_content_from_server(httpserver: HTTPServer):
+    path = "/"
+    expected_result = '{"a": 1}'
+    httpserver.expect_request(path).respond_with_data(expected_result)
+    url = httpserver.url_for(path)
+
+    reader = AsyncAPIReader()
+    result = await reader.read_url(URL(url))
+
+    assert result == expected_result
