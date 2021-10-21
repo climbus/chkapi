9170dfa03f788f937df07b29d27f4d48ccc89a7f
climbus <climbus@gmail.com>
2021-10-21 10:17:45 +0200

    show search box and hide

--- rest_checker/app.py
+++ rest_checker/app.py
@@ -16,7 +16,7 @@ from textual.widgets import Button, Footer, ScrollView
 from textual_inputs import TextInput
 
 from rest_checker.api_reader import URL, APIReader, AsyncAPIReader
-from rest_checker.events import UrlChanged
+from rest_checker.events import CancelSearch, Search, UrlChanged
 from rest_checker.exceptions import BadUrlException, HttpError
 
 
@@ -38,6 +38,26 @@ class ApiFooter(Footer):
                 ),
             )
         return content
+
+
+class CommandPrompt(TextInput):
+    def on_mount(self):
+        self.visible = False
+
+    async def show(self):
+        self.visible = True
+        await self.focus()
+
+    async def hide(self):
+        self.visible = False
+        self.value = ""
+
+    async def on_key(self, event: events.Key) -> None:
+        if event.key == "escape":
+            await self.hide()
+            await self.emit(CancelSearch(self))
+            return
+        await self.emit(Search(self, event.key))
 
 
 class URLField(TextInput):
@@ -117,8 +137,9 @@ class RestChecker(App):
     footer: ApiFooter
     url_view: URLView
     body: ScrollView
+    command_prompt: CommandPrompt
 
-    def __init__(self, url: str="", **kwargs):
+    def __init__(self, url: str = "", **kwargs):
         super().__init__(**kwargs)
         self.url = url
         self.api_reader = AsyncAPIReader()
@@ -131,9 +152,11 @@ class RestChecker(App):
         self.body = ScrollView()
         self.url_view = URLView(self.url)
         self.footer = ApiFooter()
+        self.command_prompt = CommandPrompt()
         await self.view.dock(self.url_view, size=3, edge="top")
         await self.view.dock(self.footer, edge="bottom")
         await self.view.dock(self.body, edge="top")
+        await self.view.dock(self.command_prompt, size=3, edge="bottom", z=1)
 
     async def load_url(self, url):
         try:
@@ -146,6 +169,7 @@ class RestChecker(App):
             response_time = None
 
         await self.body.update(content)
+        await self.bind("/", "search")
         self.footer.response_time = response_time
 
     async def _get_content_with_time(self, url):
@@ -163,10 +187,19 @@ class RestChecker(App):
     async def handle_url_changed(self):
         await self.load_url(self.url_view.url)
 
+    async def handle_cancel_search(self):
+        await self.body.focus()
+
+    async def on_search(self, event):
+        pass
+
     async def on_key(self, event: events.Key) -> None:
         if event.key == "ctrl+l":
             await self.url_view.focus()
         return await super().on_key(event)
+
+    async def action_search(self):
+        await self.command_prompt.show()
 
     async def _get_url_content(self, url):
         return await self.api_reader.read_url(URL(url))
--- rest_checker/events.py
+++ rest_checker/events.py
@@ -1,4 +1,24 @@
-from textual.message import Message
+from typing import Optional
+
+import rich.repr
+from textual.events import InputEvent
+from textual.message import Message, MessageTarget
+
+
+class Search(InputEvent):
+
+    __slots__ = ["key"]
+
+    def __init__(self, sender: MessageTarget, value: Optional[str]) -> None:
+        super().__init__(sender)
+        self.value = value
+
+    def __rich_repr__(self) -> rich.repr.Result:
+        yield "value", self.value
+
+
+class CancelSearch(Message):
+    pass
 
 
 class UrlChanged(Message, bubble=True):
