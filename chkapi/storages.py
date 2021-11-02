c6c06a51efa61032713154dad7c6ecd6dcd238ae
climbus <climbus@gmail.com>
2021-11-02 18:56:15 +0100

    saving unique urls

--- chkapi/storages.py
+++ chkapi/storages.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from tempfile import gettempdir
 from typing import Protocol
@@ -13,5 +14,17 @@ class Storage(Protocol):
 class TempFileStorage:
     async def save(self, url: str):
         tmpdir = Path(gettempdir())
-        with open(tmpdir / STORAGE_FILE_NAME, "a") as fp:
-            fp.write(url + "\n")
+        lines = self._load_existing_lines(tmpdir) or set()
+        lines.add(url)
+        self._write_lines(tmpdir, lines)
+
+    def _write_lines(self, tmpdir, lines):
+        with open(tmpdir / STORAGE_FILE_NAME, "w") as fp:
+            fp.write("\n".join(sorted(lines)))
+
+    def _load_existing_lines(self, tmpdir):
+        if os.path.exists(tmpdir / STORAGE_FILE_NAME):
+            lines = set()
+            with open(tmpdir / STORAGE_FILE_NAME, "r") as fp:
+                lines.update(fp.readlines())
+            return lines
--- tests/test_storages.py
+++ tests/test_storages.py
@@ -15,9 +15,23 @@ def set_tmpdir(tmp_path):
 @pytest.mark.asyncio
 async def test_save_url(tmp_path):
     url = "http://localhost/\n"
-    storage = TempFileStorage()
 
+    storage = TempFileStorage()
     await storage.save(url)
 
     with open(tmp_path / STORAGE_FILE_NAME) as fp:
         assert fp.readline() == url
+
+
+@pytest.mark.asyncio
+async def test_saves_only_unique_urls(tmp_path):
+    url1 = "http://localhost/"
+    url2 = "http://127.0.0.1/"
+
+    storage = TempFileStorage()
+    await storage.save(url1)
+    await storage.save(url1)
+    await storage.save(url2)
+
+    with open(tmp_path / STORAGE_FILE_NAME) as fp:
+        assert fp.read() == f"{url2}\n{url1}"
